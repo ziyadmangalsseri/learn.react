5d0911c600bc279925a0dd1d1f0bc44ddf8ae9a6
ziyad <ziyadmuhammad9696@gmail.com>
2024-10-31 10:28:58 +0530

    third commit

--- app.js
+++ app.js
@@ -1,46 +1,88 @@
-/**
- *
- *
- *
- *
- *
- *
- *
- *
- */
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { jsx } from "react/jsx-runtime";
 
-import React from "react"
-import ReactDOM from "react-dom"
+//  React Element
 
-const parent = React.createElement("div", {
-  id: "parent"
-},[ React.createElement(
-    "div",
-    {
-      id: "child",
-    },[
-    React.createElement("h1", {}, "hello iam h1 tag"),
-    React.createElement("h2",{},"hello iam h2 tag")
-    ]
-  ), React.createElement(
-    "div",
-    {
-      id: "child",
-    },[
-    React.createElement("h1", {}, "hello iam h1 tag"),
-    React.createElement("h2",{},"hello iam h2 tag")
-    ]
-  )]);
+// const parent = React.createElement("div", {
+//   id: "parent"
+// },[ React.createElement(
+//     "div",
+//     {
+//       id: "child",
+//     },[
+//     React.createElement("h1", {}, "hello iam h1 tag"),
+//     React.createElement("h2",{},"hello iam h2 tag")
+//     ]
+//   ), React.createElement(
+//     "div",
+//     {
+//       id: "child",
+//     },[
+//     React.createElement("h1", {}, "hello iam h1 tag"),
+//     React.createElement("h2",{},"hello iam h2 tag")
+//     ]
+//   )]);
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "headtag",
-    zzz: "yyyy",
-  },
-  "hello world"
+
+
+
+
+// React Element
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "headtag",
+//     zzz: "yyyy",
+//   },
+//   "hello world"
+// );
+
+
+
+
+
+
+// JSX - html like or XML like synatx
+
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    this is jsx{" "}
+  </h1>
 );
-console.log(parent);
+console.log(jsxHeading);
+
+
+
+
+
+
+
+// React Element
+
+const heading = <h1 id="hading">This React Element</h1>;
+
+
+
+// React Functional  Component
+
+const Title = () => (
+  <h1 id="title">
+    This is Titile 🚀
+    </h1>
+)
+
+
+    // Component Composition
+const HeadingComponent = () => (
+  <div id="container" >
+    <h2>{100+200}</h2>
+    <Title/>
+  <h1 className="head2"> This React functional component </h1>
+  </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
